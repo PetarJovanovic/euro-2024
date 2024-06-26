e62b3872f93487ebc60deb873313ccf04b09614d
Petar Jovanovic <pecazajecar@gmail.com>
2024-06-26 14:12:07 +0200

    Added suspense

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/app/components/header';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Suspense>
+          <Header />
+          {children}
+        </Suspense>
       </body>
     </html>
   );
